a2a0d008d0af3072571fa529f7b4a06b201c0bc5
Steve <smosher2005@yahoo.com>
2017-02-10 16:07:04 -0500

    Add files via upload

--- projects/DMVPN/Common/CSRReachability.py
+++ projects/DMVPN/Common/CSRReachability.py
@@ -1,5 +1,8 @@
 import pyping
 import sys
+import time
+
+counter = 0
 
 for arg in sys.argv:
    HOST = arg
@@ -7,11 +10,18 @@ for arg in sys.argv:
 response = pyping.ping(HOST)
 
 if response.ret_code == 0:
-   print("reachable")
+   print "reachable"
    exit()
 
 while response.ret_code != 0:
    response = pyping.ping(HOST)   
-   print("waiting...")
+   print "waiting..."
 else:
-    print("The CSR1000v has initialized")
+    print "The CSR1000v has initialized"
+
+
+print "The CSR1000v is now reachable. We'll wait another 3 minutes to ensure the router is fully inialized and ready for configuration."
+while counter != 180:
+  print "waiting, %d of 180 seconds......" % counter
+  time.sleep(5)
+  counter += 5
